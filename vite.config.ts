96985d1c0de996126d706e016e620d2b767a37fe
themismaconsultoria <themis.ma.consultoria@gmail.com>
2026-03-21 13:09:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/oab/", // <-- OLHA A LINHA NOVA AQUI!
   server: {
     host: "::",
     port: 8080,
